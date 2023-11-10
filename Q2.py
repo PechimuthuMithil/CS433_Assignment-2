a44fafd987b76507157f05d3c44ac7d58db2c881
KK <114857798+kaushal-003@users.noreply.github.com>
2023-11-11 00:26:15 +0530

    AlmostFinal Q2.py
    link loss added, working code

--- Q2.py
+++ Q2.py
@@ -2,13 +2,11 @@ from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.log import setLogLevel
 from mininet.cli import CLI
-from mininet.term import makeTerm
+import sys
 
-server_command = "iperf -s -p 5001,5002,5003 -t 20 "
-client_command = None
 
 class NetworkTopo(Topo):
-    def __init__(self):
+    def __init__(self,link_loss=None):
         Topo.__init__(self)
         s1, s2 = [self.addSwitch(s) for s in ('s1', 's2')]
         h1 = self.addHost(name='h1', ip='192.168.1.100/24')
@@ -17,35 +15,11 @@ class NetworkTopo(Topo):
         h4 = self.addHost(name='h4', ip='192.168.1.103/24')
         for h, s in [(h1, s1), (h2, s1), (h3, s2), (h4, s2)]:
             self.addLink(h, s)
-        self.addLink(s1, s2)
+        self.addLink(s1, s2,loss=link_loss)
 
-def function4(net, iperf_port):
-    global server_command
-    makeTerm(node=net['h4'], cmd=f"{server_command} ")
 
-def function1(net, server_ip, iperf_port, congestion_scheme):
-    global client_command
-    client_command = f"iperf -c {server_ip} -p {iperf_port} -Z {congestion_scheme} -l 4K -i 10 " \
-                     f"> client_output1.txt 2>&1 && " \
-                     f"tcpdump -i 20 -w client1_capture.pcap "
-    makeTerm(node=net['h1'], cmd=client_command)
-
-def function2(net, server_ip, iperf_port, congestion_scheme):
-    global client_command
-    client_command = f"iperf -c {server_ip} -p {iperf_port} -Z {congestion_scheme} " \
-                     f"> client_output2.txt 2>&1 " \
-                     f"tcpdump -i 20 -w client2_capture.pcap & "
-    makeTerm(node=net['h2'], cmd=client_command)
-
-def function3(net, server_ip, iperf_port, congestion_scheme):
-    global client_command
-    client_command = f"iperf -c {server_ip} -p {iperf_port} -Z {congestion_scheme} " \
-                     f"> client_output3.txt 2>&1  " \
-                     f"tcpdump -i 20 -w client3_capture.pcap & "
-    makeTerm(node=net['h3'], cmd=client_command)
-
-def run(config, congestion_scheme):
-    topo = NetworkTopo()
+def run(config, congestion_scheme,linkloss):
+    topo = NetworkTopo(link_loss=linkloss)
     net = Mininet(topo)
     net.start()
     if config == 'b':
@@ -53,30 +27,43 @@ def run(config, congestion_scheme):
         h4 = net.get('h4')
         server_ip = h4.IP()
         iperf_port = 5001
-        function4(net, iperf_port)
-        b=True
-        while not b:
-            if(b):
-                break
-        function1(net, server_ip, iperf_port, congestion_scheme)
+        net['h4'].cmd("iperf -s -p 5001,5002,5003 &")
+
+        net['h4'].cmd("timeout 15000 tcpdump -i h4-eth0 -w server_h4.pcap &")
+        net['h1'].cmd("timeout 10000 tcpdump -i h1-eth0 -w c1ient_h1.pcap &") 
+
+        net['h1'].cmd("iperf -c 192.168.1.103 -p 5001 -t 5 -Z {} &".format(congestion_scheme))
         
     elif config == 'c':
         h4 = net.get('h4')
         server_ip = h4.IP()
         iperf_port = 5001
-        function4(net, iperf_port)
-        b=True
-        while not b:
-            if(b):
-                break
-        function1(net, server_ip, iperf_port, congestion_scheme)
-        function2(net, server_ip, iperf_port, congestion_scheme)
-        function3(net, server_ip, iperf_port, congestion_scheme)
+        net['h4'].cmd("iperf -s -p 5001,5002,5003 &")
+
+        net['h4'].cmd("timeout 10000 tcpdump -i h4-eth0 -w server_h4.pcap &")
+        net['h1'].cmd("timeout 7000 tcpdump -i h1-eth0 -w client_h1.pcap &") 
+        net['h2'].cmd("timeout 7000 tcpdump -i h2-eth0 -w client_h2.pcap &") 
+        net['h3'].cmd("timeout 7000 tcpdump -i h3-eth0 -w client_h3.pcap &") 
+
+        net['h1'].cmd("iperf -c 192.168.1.103 -p 5001 -t 5 -Z {} &".format(congestion_scheme))
+        net['h2'].cmd("iperf -c 192.168.1.103 -p 5002 -t 5 -Z {} &".format(congestion_scheme))
+        net['h3'].cmd("iperf -c 192.168.1.103 -p 5003 -t 5 -Z {} &".format(congestion_scheme))
     CLI(net)
     net.stop()
 
-a = input("Enter configuration: ")
-b = input("Enter congestion control scheme: ")
 if __name__ == '__main__':
     setLogLevel('info')
-    run(a, b)
+    n = len(sys.argv)
+    config = ''
+    congestion = ''
+    linkloss = 0
+    for i in sys.argv[1:]:
+        if i.startswith('--config='):
+            config = i[9:]
+        elif (i.startswith('--congestion=')):
+            congestion = i[13:]
+        else: # --linkloss=
+            linkloss = int(i[11:])
+    a=config
+    b=congestion
+    run(a, b,linkloss)
